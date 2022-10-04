ca6212c037cdf0833598105533ba2e1d413c83cf
João Inácio <egnawake@gmail.com>
2022-10-04 09:56:33 +0100

    Manipulate Player as class

--- ClassVsStruct/Program.cs
+++ ClassVsStruct/Program.cs
@@ -6,7 +6,31 @@ namespace ClassVsStruct
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Player p1, p2;
+
+            p1 = new Player { Health = 150, Armor = 20 };
+            p2 = p1;
+
+            Console.WriteLine("Player 1");
+            ShowPlayer(p1);
+            Console.WriteLine("Player 2");
+            ShowPlayer(p2);
+            Console.WriteLine("");
+
+            p1.Health = 60;
+            Console.WriteLine("Changed P1 health");
+            Console.WriteLine("");
+
+            Console.WriteLine("Player 1");
+            ShowPlayer(p1);
+            Console.WriteLine("Player 2");
+            ShowPlayer(p2);
+            Console.WriteLine("");
+        }
+
+        private static void ShowPlayer(Player p)
+        {
+            Console.WriteLine($"Health: {p.Health} | Armor: {p.Armor}");
         }
     }
 }
