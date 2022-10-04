2d8bafdba3896639724765735a7c6ac8470ddf65
João Inácio <egnawake@gmail.com>
2022-10-05 00:22:23 +0100

    Override Value in SettlerUnit

--- GameUnits/SettlerUnit.cs
+++ GameUnits/SettlerUnit.cs
@@ -4,6 +4,8 @@ namespace GameUnits
 {
     public class SettlerUnit : Unit
     {
+        public override float Value => 5;
+
         public SettlerUnit(int movement, int health)
             : base(movement, health)
         {
